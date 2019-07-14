c42170beda6448f15371d4e1e90dd444811821c5
Marat <yeffasol@mail.ru>
2019-07-14 14:03:03 +0300

    Updates

--- my-app/main-es5.js
+++ my-app/main-es5.js
@@ -666,7 +666,7 @@ __webpack_require__.r(__webpack_exports__);
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 var environment = {
-    production: false
+    production: true
 };
 /*
  * For easier debugging in development mode, you can import the following file
